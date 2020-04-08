4f3c50cb3877a01d4ff68780de0d0c843d24d173
Digital Changes <62726908+dchanges@users.noreply.github.com>
2020-04-07 19:44:23 -0500

    Carga del proyecto para validar
    Se carga el proyecto junto con los datos necesarios para un despliegue en Jenkins con docker

---
+++ src/test/java/co/dchanges/example/rest/DockerRest/DockerRestApplicationTests.java
@@ -0,0 +1,13 @@
+package co.dchanges.example.rest.DockerRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
