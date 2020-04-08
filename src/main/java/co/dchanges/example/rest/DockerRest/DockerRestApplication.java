336e6b3902d751a853bac54562d73260d5c24af4
Digital Changes <62726908+dchanges@users.noreply.github.com>
2020-04-08 13:14:12 -0500

    Update DockerRestApplication.java

--- src/main/java/co/dchanges/example/rest/DockerRest/DockerRestApplication.java
+++ src/main/java/co/dchanges/example/rest/DockerRest/DockerRestApplication.java
@@ -11,7 +11,7 @@ public class DockerRestApplication {
 
 	@RequestMapping("/")
 	public String home() {
-		return "Hello Docker from Digital Changes";
+		return "Hello Docker from Digital Changes V2";
 	}
 
 	public static void main(String[] args) {
